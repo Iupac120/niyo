1b79d4ef8ee0248c459c137574dc383217116216
Iupac120 <oforokpara@gmail.com>
2024-05-20 11:55:55 +0100

    added index html

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -1,16 +1,3 @@
-// const express = require('express')
-// const router = express.Router()
-// const trycatchHandler = require("../utils/trycatch")
-// const authMiddleware = require("../middleware/auth")
-// const {validateTask} = require("../middleware/validate")
-// const {getTasks,createTask,updateTask,deleteTask} = require("../controllers/taskController")
-
-// router.get("/",authMiddleware,trycatchHandler(getTasks))
-// router.post("/",authMiddleware,validateTask,trycatchHandler(createTask))
-// router.put("/",authMiddleware,validateTask,trycatchHandler(updateTask))
-// router.delete("/",authMiddleware,trycatchHandler(deleteTask))
-
-// module.exports = router
 
 const express = require('express');
 const { createTask, getTasks, updateTask, deleteTask } = require('../controllers/taskController');
